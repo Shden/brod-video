9d331a9f0697106851e28afb2150af7ef69eb242
Dennis Afanassiev <denis.afanassiev@gmail.com>
2022-12-18 23:56:05 +0400

    separated private data

--- connect.js
+++ connect.js
@@ -1,11 +1,10 @@
 import { XMLHttpRequest } from "xmlhttprequest";
 import xml2js from "xml2js";
 import { tryObtainPublicIP } from "./resolve-ip.js";
+import { autoNATURI, autoNATPort } from "./privateData.js";
 
 let xhr = new XMLHttpRequest()
 
-const autoNATURI = '/NatServer/CNatServerList.xml?SN=N124F03AR35B&CT=IOS&VER=1.11.1.6019&APPID=IOS%5FM%5FPH%5FSuperLivePlus&APPUSERID=F18B77A5AD8648649958CAE373D933B3'
-const autoNATPort = 40002
 
 xhr.open('GET', `http://c2.autonat.com:${autoNATPort}${autoNATURI}`)
 
